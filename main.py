44973a58f13c25cfd0f813a464cf4b3bfeed0608
Abdullah <57626351+Abdullah476@users.noreply.github.com>
2023-03-23 23:15:16 +0500

    Updated main.py

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import pandas as pd
 from threading import Lock
 from datetime import datetime
 from inference import inference
+from sklearn import preprocessing
+from joblib import load
+import statistics as stat
 
 app = Flask(__name__, template_folder='templates')
 socketio = SocketIO(app, cors_allowed_origins='*')
@@ -63,12 +66,60 @@ def test():
         file.close()
         df = pd.read_csv('temp.csv')
         os.remove('temp.csv')
-        print(df)
+        # print(df)
+
+        # Saving the data frame for later use
+        final_frame = df.copy()
+
+        df = df.drop(['Close'], axis=1)
+
+        # dropping the "Date" column
+        df = df.drop(['Date'], axis=1)
+        df = df.drop(['Ticker'], axis=1)
+
+        # dropping null values
+        df = df.dropna()
+
+        # Seperating the data for testing
+        df = df.values
+
+        # Data normalization (0,1)
+        df = preprocessing.normalize(df, norm='l2')
+
+        # Run model
+        clf_LR = load("MODEL_LR")
+        clf_SVR = load("MODEL_SVR")
+        clf_RFR = load("MODEL_RFR")
+
+        # clf_LR.score(X_test, y_test)
+        pred_LR = clf_LR.predict(df)
+
+        # clf_SVR.score(X_test, y_test)
+        pred_SVR = clf_SVR.predict(df)
+
+        # clf_RFR.score(X_test, y_test)
+        pred_RFR = clf_RFR.predict(df)
+
+        #Use majority voting for prediction
+        predictions=pd.DataFrame([pred_LR, pred_SVR, pred_RFR])
+        final = predictions.apply(stat.mode)
+
+        # Recommendation
+        # print("Based on my training, I recommend the following")
+        # Python program to understand, how to print tables using pandas data frame
+        data = {'Estimator 1':pred_LR,'Estimator 2':pred_SVR,'Estimator 3':pred_RFR, 'Predicted Close':final}
+        data = pd.DataFrame(data)
+        # print(data)
+
+        final_frame = final_frame.join(data['Predicted Close'])
+
+
+
         # Now use that CSV to test the values, and send answer as CSV as well
         return render_template(
             'predict.html',
-            tables=[df.to_html(classes='data')],
-            titles=df.columns.values
+            tables=[final_frame.to_html(classes='data')],
+            titles=final_frame.columns.values
         )
 
 @app.route('/predict',methods=["GET","POST"])
